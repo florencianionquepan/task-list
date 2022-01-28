7f07c42f84fb70d6d42c25ec2cc9040f2caf40c7
florencianionquepan <85314154+florencianionquepan@users.noreply.github.com>
2022-01-28 17:36:14 -0300

    Se crea el servicio de tasks

--- task-list/src/app/components/tasks/tasks.component.ts
+++ task-list/src/app/components/tasks/tasks.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit} from '@angular/core';
-import { Task } from '../../task';
-import {TASKS} from '../../mock-tasks';
-
+import { TaskService } from '../../service/task.service';
+import {Task} from '../../task'
 
 @Component({
   selector: 'app-tasks',
@@ -9,11 +8,14 @@ import {TASKS} from '../../mock-tasks';
   styleUrls: ['./tasks.component.css']
 })
 export class TasksComponent implements OnInit {
-  tasks: Task[] = TASKS
+  tasks: Task[] = [];
   
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private taskService: TaskService
+  ) { }
+  
+  ngOnInit(): void { //cuando se monte el componente
+    this.tasks = this.taskService.getTasks();
   }
 
 }
