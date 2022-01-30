e9f255a3c7f2c5e6313dc98e0c3f2436db595721
florencianionquepan <85314154+florencianionquepan@users.noreply.github.com>
2022-01-30 13:21:03 -0300

    Se crea showAddTask

--- task-list/src/app/components/header/header.component.ts
+++ task-list/src/app/components/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { UiService } from 'src/app/service/ui.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-header',
@@ -8,12 +10,20 @@ import { Component, OnInit } from '@angular/core';
 export class HeaderComponent implements OnInit {
 
   title:string = 'My list of tasks';
-  constructor() { }
+  showAddTask:boolean=true;
+  subscription?: Subscription;
+  constructor(
+    private uiService:UiService
+  ) {
+    this.subscription = this.uiService.onToggle().subscribe(value =>{
+      this.showAddTask =value
+    })
+  }
 
   ngOnInit(): void {
   }
 
   toggleAddTasks(){
-    
+    this.uiService.toggleAddTask();
   }
 }
